9c41e036fd30667a10efb062810cec9bd11dfa23
Booxxyy <andrent20@mail.ru>
2025-09-03 01:10:33 +0500

    Theme

--- app/src/main/java/com/stargalaxy/spacetraveler/ui/theme/Theme.kt
+++ app/src/main/java/com/stargalaxy/spacetraveler/ui/theme/Theme.kt
@@ -12,15 +12,25 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = PrimaryColor,
+    onPrimary = OnPrimaryColor,
+    secondary = SecondaryColor,
+    onSecondary = OnSecondaryColor,
+    surface = SurfaceColor,
+    onSurface = OnSurfaceColor,
+    background = BackgroundColor,
+    onBackground = OnBackgroundColor,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = PrimaryColor,
+    onPrimary = OnPrimaryColor,
+    secondary = SecondaryColor,
+    onSecondary = OnSecondaryColor,
+    surface = SurfaceColor,
+    onSurface = OnSurfaceColor,
+    background = BackgroundColor,
+    onBackground = OnBackgroundColor,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
--- app/src/main/java/com/stargalaxy/spacetraveler/ui/theme/Type.kt
+++ app/src/main/java/com/stargalaxy/spacetraveler/ui/theme/Type.kt
@@ -1,15 +1,25 @@
 package com.stargalaxy.spacetraveler.ui.theme
 
+import androidx.compose.ui.text.font.Font
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.stargalaxy.spacetraveler.R
+
+val fonts = FontFamily(
+    Font(resId = R.font.roboto_regular, weight = FontWeight.Normal),
+    Font(resId = R.font.roboto_medium, weight = FontWeight.Medium),
+    Font(resId = R.font.roboto_light, weight = FontWeight.Light),
+    Font(resId = R.font.roboto_bold, weight = FontWeight.Bold)
+)
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fonts,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
